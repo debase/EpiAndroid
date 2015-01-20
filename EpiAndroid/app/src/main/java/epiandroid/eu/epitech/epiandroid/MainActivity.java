f29ed2c743513ca96b58af5ba6c40b78fbd9a787
Remi Hillairet <remi.hillairet@epitech.eu>
2015-01-20 13:43:37 +0100

    EditText added + Login works

--- EpiAndroid/app/src/main/java/epiandroid/eu/epitech/epiandroid/MainActivity.java
+++ EpiAndroid/app/src/main/java/epiandroid/eu/epitech/epiandroid/MainActivity.java
@@ -4,33 +4,31 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private EditText loginField;
+    private EditText passwordField;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        JsonObject json = new JsonObject();
-        json.addProperty("login", "debas_e");
-        json.addProperty("password", "X*.KEi^0");
-
-        System.out.println(json);
-        Ion.with(this).load("http://epitech-api.herokuapp.com/login/")
-                .setJsonObjectBody(json)
-                .asJsonObject()
-                .setCallback(new FutureCallback<JsonObject>() {
-                    @Override
-                    public void onCompleted(Exception e, JsonObject result) {
-                            System.out.println(result.toString());
-                    }
-                });
+        this.loginField = (EditText)findViewById(R.id.loginField);
+        this.passwordField = (EditText)findViewById(R.id.passwordField);
+        this.loginButton = (Button)findViewById(R.id.loginButton);
+        this.loginButton.setOnClickListener(this);
     }
 
 
@@ -54,5 +52,32 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.loginButton) {
+            Ion.with(this).load("http://epitech-api.herokuapp.com/login")
+                    .setBodyParameter("login", loginField.getText().toString())
+                    .setBodyParameter("password", passwordField.getText().toString())
+                    .asString()
+                    .setCallback(new FutureCallback<String>() {
+                        @Override
+                        public void onCompleted(Exception e, String result) {
+                            JSONObject objectResult = null;
+                            try {
+                                objectResult = new JSONObject(result);
+                            } catch (JSONException e1) {
+                                e1.printStackTrace();
+                            }
+                            try {
+                                String token = objectResult.getString("token");
+                                System.out.println("Token : " + token);
+                            } catch (JSONException e1) {
+                                e1.printStackTrace();
+                            }
+                        }
+                    });
+        }
     }
 }
