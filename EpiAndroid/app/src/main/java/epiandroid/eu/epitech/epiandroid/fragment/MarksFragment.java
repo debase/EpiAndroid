e0ca739176404b2d654711784886b498f4898e25
fabien.linardon <fabien.linardon@epitech.eu>
2015-01-28 19:41:15 +0100

    new abstract for handle responseHandler with full template

--- EpiAndroid/app/src/main/java/epiandroid/eu/epitech/epiandroid/fragment/MarksFragment.java
+++ EpiAndroid/app/src/main/java/epiandroid/eu/epitech/epiandroid/fragment/MarksFragment.java
@@ -2,7 +2,6 @@ package epiandroid.eu.epitech.epiandroid.fragment;
 
 import android.os.Bundle;
 import android.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +20,9 @@ import java.util.Arrays;
 
 import epiandroid.eu.epitech.epiandroid.R;
 import epiandroid.eu.epitech.epiandroid.adapter.MarksViewAdapter;
+import epiandroid.eu.epitech.epiandroid.epitech_service.GsonResponseHandler;
 import epiandroid.eu.epitech.epiandroid.epitech_service.EpitechService;
+import epiandroid.eu.epitech.epiandroid.model.MarkModel;
 import epiandroid.eu.epitech.epiandroid.model.MarksItem;
 
 
@@ -29,18 +30,15 @@ public class MarksFragment extends Fragment {
 
     private View view;
 
-    /*private JsonHttpResponseHandler jsonHttpResponseHandler = new GsonHttpResponseHandler<Model>(Model[].class) {
-        @Override
-        public void onSuccess(int statusCode, Header[] headers, Model responseString) {
-            super.onSuccess(statusCode, headers, responseString);
-
-        }
+    private GsonResponseHandler<MarkModel> gsonResponseHandler = new GsonResponseHandler<MarkModel>(MarkModel.class) {
 
         @Override
-        public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-            super.onFailure(statusCode, headers, responseString, throwable);
+        public void onSuccess(MarkModel markModel) {
+            MarksViewAdapter adapter = new MarksViewAdapter(getActivity(), R.layout.mark_item, new ArrayList<MarksItem>(Arrays.asList(markModel.getMarksItem())));
+            ListView listView = (ListView) view.findViewById(R.id.mark_list);
+            listView.setAdapter(adapter);
         }
-    }*/
+    };
 
     private JsonHttpResponseHandler jsonHttpResponseHandler = new JsonHttpResponseHandler() {
         @Override
@@ -67,7 +65,7 @@ public class MarksFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        EpitechService.getRequest("marks", null, jsonHttpResponseHandler);
+        EpitechService.getRequest("marks", null, gsonResponseHandler);
     }
 
     @Override
