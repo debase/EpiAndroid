7f9cddea0d4b800b3a089d5ffc0d46462fd8b346
Remi Hillairet <remi.hillairet@epitech.eu>
2015-02-02 14:20:43 +0100

    Planning Adapter + layout added

--- EpiAndroid/app/src/main/java/epiandroid/eu/epitech/epiandroid/adapter/PlanningAdapter.java
+++ EpiAndroid/app/src/main/java/epiandroid/eu/epitech/epiandroid/adapter/PlanningAdapter.java
@@ -1,10 +1,16 @@
 package epiandroid.eu.epitech.epiandroid.adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.List;
@@ -17,7 +23,7 @@ import epiandroid.eu.epitech.epiandroid.model.PlanningItem;
 /**
  * Created by remihillairet on 28/01/15.
  */
-public class PlanningAdapter extends ArrayAdapter<PlanningItem> {
+public class PlanningAdapter extends ArrayAdapter<PlanningItem> implements View.OnClickListener {
 
     private int resource;
 
@@ -41,6 +47,7 @@ public class PlanningAdapter extends ArrayAdapter<PlanningItem> {
         if (planningItem != null) {
             TextView titleLabel = (TextView) v.findViewById(R.id.adapterPlanningTitle);
             TextView scheduleLabel = (TextView) v.findViewById(R.id.adapterPlanningSchedule);
+            Button btnToken = (Button) v.findViewById(R.id.btnToken);
 
             if (titleLabel != null) {
                 titleLabel.setText(planningItem.getTitle());
@@ -49,8 +56,30 @@ public class PlanningAdapter extends ArrayAdapter<PlanningItem> {
             if (scheduleLabel != null) {
                 scheduleLabel.setText(planningItem.getSchedule());
             }
+
+            if (btnToken != null) {
+                btnToken.setOnClickListener(this);
+            }
         }
 
         return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        final EditText input = new EditText(getContext());
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        new AlertDialog.Builder(getContext())
+                .setTitle("Enter your token")
+                .setView(input)
+                .setPositiveButton("Validate", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Send token
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Do nothing.
+            }
+        }).show();
     }
 }
